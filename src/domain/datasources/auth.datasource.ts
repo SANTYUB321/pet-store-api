863e3148c244ba5a48e3bd49e53c31763d8ec265
SANTYUB321 <santiagouribebohorquez111@gmail.com>
2024-05-07 23:25:05 -0500

    Generacion de Datasources y repository

---
+++ src/domain/datasources/auth.datasource.ts
@@ -0,0 +1,9 @@
+
+import { RegisterUserDto } from '../dtos/auth/register-user-dto';
+import { UserEntity } from '../entities/auth/user.entity';
+
+
+export abstract class AuthDataSource { // 1
+    abstract register(registerUserDto:RegisterUserDto): Promise<UserEntity> //4
+
+} 
---
+++ src/domain/repositories/auth.repository.ts
@@ -0,0 +1,9 @@
+import { RegisterUserDto } from "../dtos/auth/register-user-dto";
+import { UserEntity } from "../entities/auth/user.entity";//2
+
+
+
+export abstract class AuthRepository { // 1
+    abstract register(registerUserDto:RegisterUserDto): Promise<UserEntity> //4
+
+} 
